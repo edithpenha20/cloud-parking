cc27cd109421283136ea7cf48e37d7a30318e46d
Edith Penha <edithpenha@gmail.com>
2021-09-19 16:24:35 -0400

    Primary endpoint List

---
+++ src/main/java/com/parking/controller/ParkingController.java
@@ -0,0 +1,31 @@
+package com.parking.controller;
+
+import com.parking.controller.dto.ParkingDTO;
+import com.parking.controller.mapper.ParkingMapper;
+import com.parking.model.Parking;
+import com.parking.service.ParkingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper){
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    @GetMapping
+    public List<ParkingDTO> findAll(){
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
+    }
+}
