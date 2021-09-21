77249dc9cdf7bf15650b0dd5b73da96ba2b91de0
Edith Penha <edithpenha@gmail.com>
2021-09-21 18:49:22 -0400

    swagger and controller changes

--- src/main/java/com/parking/controller/ParkingController.java
+++ src/main/java/com/parking/controller/ParkingController.java
@@ -5,6 +5,8 @@ import com.parking.controller.dto.ParkingDTO;
 import com.parking.controller.mapper.ParkingMapper;
 import com.parking.model.Parking;
 import com.parking.service.ParkingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
     private final ParkingService parkingService;
@@ -24,6 +27,7 @@ public class ParkingController {
     }
 
     @GetMapping
+    @ApiOperation("Find all parkings")
     public ResponseEntity<List<ParkingDTO>> findAll(){
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
@@ -59,7 +63,7 @@ public class ParkingController {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/{id}")
+    @PostMapping("/{id}/exit")
     public ResponseEntity<ParkingDTO> checkout(@PathVariable String id) {
         Parking parking = parkingService.checkout(id);
         return ResponseEntity.ok(parkingMapper.toParkingDTO(parking));
